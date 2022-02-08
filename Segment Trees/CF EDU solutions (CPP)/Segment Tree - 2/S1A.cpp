4c36ed410961c1f382b8259d9a6648d75526ea3e
SabreDev <201801074@daiict.ac.in>
2022-02-08 18:47:58 +0530

    Adding Segment Tree Guide

---
+++ Segment Trees/CF EDU solutions (CPP)/Segment Tree - 2/S1A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll  long long
+#define all(v) v.begin(),v.end()
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rrep(i,n) for(int i=n-1;i>=0;--i)
+#define rng(i,a,b) for(int i=a;i<b;i++)
+#define ar array
+
+int main(){
+	fast;
+}
