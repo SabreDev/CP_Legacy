a297af7c5050f16eddceb86fdde01128d0bbb620
Ritik Malaviya <ritikmalaviya@Ritiks-MacBook-Pro.local>
2024-01-08 00:19:12 +0530

    Adds dp problems

---
+++ DP/Codeforces/ValuesYouCanMake.cpp
@@ -0,0 +1,28 @@
+//https://codeforces.com/contest/687/problem/C
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define all(v) v.begin(),v.end()
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rrep(i,n) for(int i=n-1;i>=0;--i)
+#define rng(i,a,b) for(int i=a;i<b;i++)
+#define ar array
+
+void run_case(){
+
+}
+
+int main(){
+	fast;
+	int t=1;
+	while(t--){
+    run_case();
+	}
+}
